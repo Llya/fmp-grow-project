c2b9223a976fd990d378d2707bdc9b88a0c209de
Llya <Llya@users.noreply.github.com>
2018-01-28 13:35:20 +0000

    wheel functionality
    ability to display random restaurant on page

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-var restaurants = ["Franco's", "Homeslice","Nando's","McDonald's","Pret","Bottega","Flat Iron"]
+var restaurants = ["Franco's", "Homeslice","Nando's","McDonald's","Pret","Bottega","Flat Iron","Blue's Kitchen", "The Diner"]
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
-function triggerWheel(element) {
-    console.log(getRandomInt(restaurants.length));
-    console.log(element.className)
-    element.className.hidden = true;
+function triggerWheel() {
+    var trigger = document.getElementById ("trigger") ;
+    var restaurant = document.getElementById ("restaurant") ;
+    var result = getRandomInt(restaurants.length);
+    var reload = document.getElementById ("reload") ;
+    trigger.style.visibility = "hidden";
+    restaurant.innerHTML = restaurants[result];
+    restaurant.style.visibility = "visible";
+    reload.style.visibility = "visible"
+
+}
+
+function triggerReload() {
+    var restaurant = document.getElementById ("restaurant") ;
+    var result = getRandomInt(restaurants.length);
+    var reload = document.getElementById ("reload") ;
+    restaurant.innerHTML = restaurants[result];
 }
