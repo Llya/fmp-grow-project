1963998de6c63d8fd657c015441e6a7529e319e9
Llya <Llya@users.noreply.github.com>
2018-02-04 22:44:41 +0000

    limit of 3 goes with no repeats

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-/*
-remove onclick and use event listener
-use display: none instead of visibility: hidden
-use 1 button instead of 2 (change the text instead of hiding it)
-don't return same restaurant twice 
-*/
-
 var restaurants = ["Franco's", "Homeslice", "Nando's", "McDonald's", "Pret", "Bottega", "Flat Iron", "Blue's Kitchen", "The Diner"]
+
+function displayList(item, listId) {
+    var list = document.getElementById(listId);
+    var list_display="";
+    for (var i = 0; i < item.length; i++) {
+        list_display += "<li>" + item[i] + "</li>";
+    }
+    list.innerHTML = list_display;
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
 var trigger = document.getElementById("trigger");
+var options = [];
 
 trigger.addEventListener("click", function () {
+
+    if (options.length < 3) {
     //get a number at random
     var result = getRandomInt(restaurants.length);
-    //get answer text
-    var answer = document.getElementById("restaurant");
+    
     //update text and display
+    var answer = document.getElementById("restaurant");
     answer.innerHTML = restaurants[result];
     answer.style.display = "block";
+
+    //store result
+    options.push(restaurants[result]);
+
+    //remove result from array
+    restaurants.splice(result,1);
+
     //change button text
-    trigger.innerHTML = "Not happy? Try again."
-    trigger.style.fontSize ="15px"
+    trigger.innerHTML = "Not happy? Try again.";
+    trigger.style.fontSize ="15px";
+
+    } else {
+    var wheel = document.getElementById("wheel");
+    var limit = document.getElementById("limit");
+    wheel.style.display = "none";
+    limit.style.display = "block";
+    displayList(options, "optionsList");
+    }
 })
