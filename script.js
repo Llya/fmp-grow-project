7b69d355a8db1f477d89c732489b517509a90527
Llya <Llya@users.noreply.github.com>
2018-03-04 20:26:24 +0000

    filtering on location

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
 //DISPLAYING LIST OF RESTAURANTS
 function displayList(item, listId) {
     var list = document.getElementById(listId);
-    var list_display="";
+    var list_display = "";
     for (var i = 0; i < item.length; i++) {
         list_display += "<li>" + item[i].name + "</li>";
     }
     list.innerHTML = list_display;
 }
+
+//TAKING INTO ACCOUNT FILTERS SELECTED
+var location_filter = document.getElementById("location");
+
+location_filter.addEventListener("change", function () {
+    var filtered_list = [];
+    if (document.getElementById("location-1").checked) {
+        //CHECK EACH VALUE FOR MATCH
+        for (var i = 0; i < restaurants.length; i++) {
+            if (restaurants[i].location === 1) {
+                //CHECK VALUE ISN'T ALREADY IN LIST
+                var matches = 0;
+                for (var ii = 0; ii < filtered_list.length; ii++) {
+                    if (filtered_list[ii].name === restaurants[i].name) {
+                        matches = 1;
+                        break;
+                    }
+                }
+                if (matches !== 1) {
+                    filtered_list.push(restaurants[i])
+                }
+            }
+        }
+    } else if (document.getElementById("location-2").checked) {
+        for (var i = 0; i < restaurants.length; i++) {
+            if (restaurants[i].location <= 2) {
+                //CHECK VALUE ISN'T ALREADY IN LIST
+                var matches = 0;
+                for (var ii = 0; ii < filtered_list.length; ii++) {
+                    if (filtered_list[ii].name === restaurants[i].name) {
+                        matches = 1;
+                        break;
+                    }
+                }
+                if (matches !== 1) {
+                    filtered_list.push(restaurants[i])
+                }
+            }
+        }
+    }
+    displayList(filtered_list, "restaurantList")
+})
 
 
 //GENERATING RESTAURANT OPTIONS
@@ -20,30 +62,30 @@ var options = [];
 trigger.addEventListener("click", function () {
 
     if (options.length < 3) {
-    
-    //get a number at random
-    var result = getRandomInt(restaurants.length);
-    
-    //update text and display
-    var answer = document.getElementById("restaurant");
-    answer.innerHTML = restaurants[result].name;
-    answer.style.display = "block";
 
-    //store result
-    options.push(restaurants[result]);
+        //get a number at random
+        var result = getRandomInt(restaurants.length);
 
-    //remove result from array
-    restaurants.splice(result,1);
+        //update text and display
+        var answer = document.getElementById("restaurant");
+        answer.innerHTML = restaurants[result].name;
+        answer.style.display = "block";
 
-    //change button text
-    trigger.innerHTML = "Not happy? Try again.";
-    trigger.style.fontSize ="15px";
+        //store result
+        options.push(restaurants[result]);
+
+        //remove result from array
+        restaurants.splice(result, 1);
+
+        //change button text
+        trigger.innerHTML = "Not happy? Try again.";
+        trigger.style.fontSize = "15px";
 
     } else {
-    var wheel = document.getElementById("wheel");
-    var limit = document.getElementById("limit");
-    wheel.style.display = "none";
-    limit.style.display = "block";
-    displayList(options, "optionsList");
+        var wheel = document.getElementById("wheel");
+        var limit = document.getElementById("limit");
+        wheel.style.display = "none";
+        limit.style.display = "block";
+        displayList(options, "optionsList");
     }
 })
